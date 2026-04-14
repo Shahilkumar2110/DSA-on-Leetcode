cea8fd6ac78f59795a8a1558ee58f4b9901100c8
shahil <shahil.kumar.cs28@iilm.edu>
2026-04-14 14:07:34 +0530

    Time: 5 ms (68.51%), Space: 43.5 MB (48.1%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,44 +1,15 @@
 class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
-        int n=citations.length;
+        int n = citations.length;
 
-        int max=citations.length;
-        int min=0;
-        int ans=-1;
-
-        while(min<=max){
-            int mid=min+(max-min)/2;
-
-            int count=n-funct(citations,mid);
-
-            if(count>=mid){
-                ans=mid;
-                min=mid+1;
-            }else{
-                max=mid-1;
+        for (int i = 0; i < n; i++) {
+            int h = n - i;
+            if (citations[i] >= h) {
+                return h;
             }
-
         }
-        return ans;
-        
-    }
 
-    int funct(int[] citations,int mid){
-        int low=0;
-        int high=citations.length-1;
-        int ans=citations.length;
-        while(low<=high){
-            int mid2=low+(high-low)/2;
-
-            if(citations[mid2]>=mid){
-                ans=mid2;
-                high=mid2-1;
-            }else{
-                low=mid2+1;
-            }
-
-        }
-        return ans;
+        return 0;
     }
 }
