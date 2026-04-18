b458b078c504986f770efcd29d51f6a1149b2e38
shahil <shahil.kumar.cs28@iilm.edu>
2026-04-18 10:55:01 +0530

    Time: 1 ms (100%), Space: 42.3 MB (96.09%) - LeetHub

---
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int mirrorDistance(int n) {
+        int reverse=0;
+        int num=n;
+
+        while(n!=0){
+            reverse=(reverse*10)+(n%10);
+            n/=10;
+        }
+        return Math.abs(num-reverse);
+    }
+}
