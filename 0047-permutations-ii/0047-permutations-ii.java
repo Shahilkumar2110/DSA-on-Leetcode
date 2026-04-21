e3fdaf338f414bb3c04d078d0f3e51fc9d02c474
shahil <shahil.kumar.cs28@iilm.edu>
2026-04-21 18:12:22 +0530

    Time: 696 ms (5.05%), Space: 45.9 MB (99.51%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result=new ArrayList<>();
+        boolean[] used=new boolean[nums.length];
+
+        funct(result, used, nums, new ArrayList<>());
+        return result;
+    }
+    void funct(List<List<Integer>> result , boolean[] used , int[] nums, List<Integer> curr){
+        if(curr.size()==nums.length){
+            if(!result.contains(curr)){
+                result.add(new ArrayList<>(curr));
+            }
+            return ;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(used[i]) continue;
+
+            used[i]=true;
+            curr.add(nums[i]);
+
+            funct(result, used , nums, curr);
+
+            used[i]=false;
+            curr.remove(curr.size()-1);
+        }
+    }
+}
