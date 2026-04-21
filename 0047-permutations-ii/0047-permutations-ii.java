d121beca625ba58525814a0df6d0220e769a2ae6
shahil <shahil.kumar.cs28@iilm.edu>
2026-04-21 18:16:02 +0530

    Time: 35 ms (9.85%), Space: 47.2 MB (7.78%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,29 +1,31 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
-        boolean[] used=new boolean[nums.length];
+        Set<List<Integer>> set = new HashSet<>();
+        boolean[] used = new boolean[nums.length];
 
-        funct(result, used, nums, new ArrayList<>());
-        return result;
+        funct(set, used, nums, new ArrayList<>());
+
+        return new ArrayList<>(set);
     }
-    void funct(List<List<Integer>> result , boolean[] used , int[] nums, List<Integer> curr){
-        if(curr.size()==nums.length){
-            if(!result.contains(curr)){
-                result.add(new ArrayList<>(curr));
-            }
-            return ;
+
+    void funct(Set<List<Integer>> set, boolean[] used, int[] nums, List<Integer> curr) {
+
+        if (curr.size() == nums.length) {
+            set.add(new ArrayList<>(curr)); 
+            return;
         }
 
-        for(int i=0;i<nums.length;i++){
-            if(used[i]) continue;
+        for (int i = 0; i < nums.length; i++) {
 
-            used[i]=true;
+            if (used[i]) continue;
+
+            used[i] = true;
             curr.add(nums[i]);
 
-            funct(result, used , nums, curr);
+            funct(set, used, nums, curr);
 
-            used[i]=false;
-            curr.remove(curr.size()-1);
+            used[i] = false;
+            curr.remove(curr.size() - 1);
         }
     }
 }
