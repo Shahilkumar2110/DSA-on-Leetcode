13d6ff8e4cfaa95f514f1a03464d1a99468c8005
shahil <shahil.kumar.cs28@iilm.edu>
2026-04-27 02:24:56 +0530

    Time: 8 ms (18.6%), Space: 45 MB (48.15%) - LeetHub

---
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValidSerialization(String preorder) {
+
+        String[] arr = preorder.split(",");
+        Stack<pair> st = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            String ch = arr[i];
+
+
+            while (ch.equals("#") && !st.isEmpty() && st.peek().child == 1) {
+                st.pop();
+            }
+
+            if (ch.equals("#")) {
+                if (st.isEmpty()) return i == arr.length - 1;
+                st.peek().child++;
+            } else {
+                st.push(new pair(0)); 
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
+
+class pair {
+    int child;
+
+    pair(int c) {
+        this.child = c;
+    }
+}
