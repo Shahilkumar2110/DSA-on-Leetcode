9eb8f1e66c5fbbab7a8a88f12920f6b6bbdf1846
shahil <shahil.kumar.cs28@iilm.edu>
2026-05-03 01:14:29 +0530

    Time: 0 ms (100%), Space: 47.1 MB (16.89%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -15,36 +15,47 @@
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        TreeNode result=merge(root1,root2);
-        return result;
+
+        if(root1==null) return root2;
+        if(root2==null) return root1;
+
+        TreeNode merge = new TreeNode(root1.val+root2.val);
+        merge.left=mergeTrees(root1.left, root2.left);
+        merge.right=mergeTrees(root1.right, root2.right);
+        return merge;
+
+
+
+        // TreeNode result=merge(root1,root2);
+        // return result;
     }
-    TreeNode merge(TreeNode root1, TreeNode root2){
-        if(root1==null && root2==null){
-            return null;
-        }
-        int val1=root1==null?0:root1.val;
-        int val2=root2==null?0:root2.val;
+    // TreeNode merge(TreeNode root1, TreeNode root2){
+    //     if(root1==null && root2==null){
+    //         return null;
+    //     }
+    //     int val1=root1==null?0:root1.val;
+    //     int val2=root2==null?0:root2.val;
 
-        TreeNode result=new TreeNode(val1+val2);
+    //     TreeNode result=new TreeNode(val1+val2);
 
-        if(root1==null && root2!=null){
+    //     if(root1==null && root2!=null){
             
-        result.left=merge(root1, root2.left);
-        result.right=merge(root1, root2.right);
+    //     result.left=merge(root1, root2.left);
+    //     result.right=merge(root1, root2.right);
 
-        }else if(root2==null && root1!=null){
+    //     }else if(root2==null && root1!=null){
             
-        result.left=merge(root1.left, root2);
-        result.right=merge(root1.right, root2);
+    //     result.left=merge(root1.left, root2);
+    //     result.right=merge(root1.right, root2);
 
-        }else{
+    //     }else{
         
-        result.left=merge(root1.left, root2.left);
-        result.right=merge(root1.right, root2.right);
+    //     result.left=merge(root1.left, root2.left);
+    //     result.right=merge(root1.right, root2.right);
 
-        }
+    //     }
 
 
-        return result;
-    }
+    //     return result;
+    // }
 }
