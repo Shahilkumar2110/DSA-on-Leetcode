8a86f810bd484f3ff1352ba4419ecccf5161f411
shahil <shahil.kumar.cs28@iilm.edu>
2026-06-29 19:29:28 +0530

    Time: 8 ms (59.86%), Space: 46.8 MB (26.94%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,17 +1,37 @@
 class Solution {
+    int[][] arr;
+    int offset;
+
     public int findTargetSumWays(int[] nums, int target) {
-        return sumof(0,0,nums,target);
+
+        int sum=0;
+        for(int e:nums)sum+=e;
+
+        if(Math.abs(target)>sum) return 0;
+        
+        arr=new int[nums.length][2*sum+1];
+
+        for(int[] ele:arr){
+            Arrays.fill(ele,-1);
+        }
+
+        offset=sum;
+
+        return sumof(0,0,nums,target,arr);
+
     }
-    int sumof(int ind ,int sum, int[] nums, int target){
+    int sumof(int ind ,int sum, int[] nums, int target,int[][] dp){
 
         if(ind==nums.length) {
             return sum==target?1:0;
         }
 
-        int plus=sumof(ind+1,sum+nums[ind], nums,target);
-        int minus=sumof(ind+1,sum-nums[ind], nums, target);
+        if(dp[ind][offset+sum]!=-1) return dp[ind][offset+sum];
 
-        return plus+minus;
+        int plus=sumof(ind+1,sum+nums[ind], nums,target,dp);
+        int minus=sumof(ind+1,sum-nums[ind], nums, target,dp);
+
+        return dp[ind][offset+sum]=plus+minus;
 
     }
 }
