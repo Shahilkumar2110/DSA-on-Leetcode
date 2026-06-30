3816d8943edc7772725ac7e38338c5a8a3cda230
shahil <shahil.kumar.cs28@iilm.edu>
2026-07-01 00:43:20 +0530

    Time: 20 ms (56.33%), Space: 54.3 MB (64.82%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,24 +3,45 @@ class Solution {
         int n=s.length();
         int m=t.length();
 
-        int[][] dp=new int[n][m];
+        int[][] dp=new int[n+1][m+1];
 
-        for(int[] ele:dp){
-            Arrays.fill(ele,-1);
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }   
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)!=t.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j];
+                    continue;
+                } 
+
+                int take=dp[i-1][j-1];
+                int notake=dp[i-1][j];
+                dp[i][j]=take+notake;
+            }
         }
-        return funct(n-1,m-1,s,t,dp);
-    }
-    int funct(int i1, int i2, String s,String t,int[][] dp){
-        if(i2<0) return 1;
-        if(i1<0){
-            return 0;
-        }
+        return dp[n][m];
 
-        if(dp[i1][i2]!=-1) return dp[i1][i2];
-        if(s.charAt(i1)!=t.charAt(i2)) return funct(i1-1,i2,s,t,dp);
 
-        int take=funct(i1-1,i2-1,s,t,dp);
-        int notake=funct(i1-1,i2,s,t,dp);
-        return dp[i1][i2]=take+notake;
+
+
+        // for(int[] ele:dp){
+        //     Arrays.fill(ele,-1);
+        // }
+        // return funct(n-1,m-1,s,t,dp);
     }
+    // int funct(int i1, int i2, String s,String t,int[][] dp){
+    //     if(i2<0) return 1;
+    //     if(i1<0){
+    //         return 0;
+    //     }
+
+    //     if(dp[i1][i2]!=-1) return dp[i1][i2];
+    //     if(s.charAt(i1)!=t.charAt(i2)) return funct(i1-1,i2,s,t,dp);
+
+    //     int take=funct(i1-1,i2-1,s,t,dp);
+    //     int notake=funct(i1-1,i2,s,t,dp);
+    //     return dp[i1][i2]=take+notake;
+    // }
 }
