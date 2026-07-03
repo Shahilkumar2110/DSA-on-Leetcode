b06576eed1e1c9eb2950fe49aeff7f4d8a5a222c
shahil <shahil.kumar.cs28@iilm.edu>
2026-07-03 20:58:20 +0530

    Time: 86 ms (21.82%), Space: 237.3 MB (9.87%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,25 +1,44 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[][] dp=new int[nums.length][nums.length+1];
-        for(int[] ele:dp){
-            Arrays.fill(ele,-1);
+        // int[][] dp=new int[nums.length][nums.length+1];
+        // for(int[] ele:dp){
+        //     Arrays.fill(ele,-1);
+        // }
+        // return funct(0,-1 , nums,dp);
+
+        int n=nums.length;
+        int[][] dp=new int[n+1][n+1];
+
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=i-1;j>=-1;j--){
+
+                int nottake=dp[i+1][j+1];
+                int take=0;
+                if (j==-1 || nums[i] > nums[j]) {
+                    take = 1 + dp[i+1][i+1];
+                }
+                dp[i][j+1]=Math.max(nottake,take);
+
+            }
         }
-        return funct(0,-1 , nums,dp);
-    }
-    int funct(int ind, int pre, int[] nums,int[][] dp){
-        if (ind == nums.length) return 0;
-
-        if (dp[ind][pre + 1] != -1)
-            return dp[ind][pre + 1];
-
-        int notTake = funct(ind + 1, pre, nums, dp);
-
-        int take = 0;
-        if (pre == -1 || nums[ind] > nums[pre]) {
-            take = 1 + funct(ind + 1, ind, nums, dp);
-        }
-
-        return dp[ind][pre + 1] = Math.max(take, notTake);
+        return dp[0][0];
 
     }
+    // int funct(int ind, int pre, int[] nums,int[][] dp){
+    //     if (ind == nums.length) return 0;
+
+    //     if (dp[ind][pre + 1] != -1)
+    //         return dp[ind][pre + 1];
+
+    //     int notTake = funct(ind + 1, pre, nums, dp);
+
+    //     int take = 0;
+    //     if (pre == -1 || nums[ind] > nums[pre]) {
+    //         take = 1 + funct(ind + 1, ind, nums, dp);
+    //     }
+
+    //     return dp[ind][pre + 1] = Math.max(take, notTake);
+
+    // }
 }
